541c9d6e899f091e80facb1993a9f8334ed55f82
Marg2-r <ivan.antonenko22@gmail.com>
2026-07-07 18:17:08 +0200

    feat(api): initialize TableFlow API with health endpoint

---
+++ srcTableFlow.Api/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TableFlow.Api.Controllers;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            status = "Healthy",
+            service = "TableFlow API",
+            timestampUtc = DateTime.UtcNow
+        });
+    }
+}
---
+++ srcTableFlow.Api/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Tableflow API v1");
+    });
+
+}
+//Leave this disabled for now because you are running HTTP locally
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
