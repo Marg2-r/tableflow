e530569888364acd4fc7a84ad05c7ec2d87d837d
Marg2-r <ivan.antonenko22@gmail.com>
2026-07-12 12:24:45 +0200

    Dockerize API and PostgreSQL

--- srcTableFlow.Api/Program.cs
+++ srcTableFlow.Api/Program.cs
@@ -20,6 +20,14 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider
+        .GetRequiredService<TableFlowDbContext>();
+
+    await dbContext.Database.MigrateAsync();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
